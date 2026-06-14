95fc964fa4b9822ad0299c1b8c10f796e57bf42b
uc-mechno <mechnotechno@gmail.com>
2026-06-14 18:06:09 +0900

    アコーディオン、モーダル、スライダーのインポートを削除し、common.jsを追加してグローバル定数を設定

--- assets/js/_main.js
+++ assets/js/_main.js
@@ -1,16 +1,9 @@
-import { accordion } from './_accordion.js';
-import { modal } from './_modal.js';
-import { slider } from './_slider.js';
 import { viewportSwitch } from './_viewportSwitch.js';
 import { updateCopyright } from './_updateCopyright.js';
 
 window.addEventListener('load', function () {
-  accordion();
   updateCopyright();
   viewportSwitch();
 });
 
-document.addEventListener('DOMContentLoaded', function () {
-  modal();
-  slider();
-});
+document.addEventListener('DOMContentLoaded', function () {});
